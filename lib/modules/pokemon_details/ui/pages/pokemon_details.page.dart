5d4ded26545435f75d491befbf653077044ff12f
danipishinin <danielepishinin@gmail.com>
2021-12-20 17:24:10 -0300

    feat: details

--- lib/modules/pokemon_details/ui/pages/pokemon_details.page.dart
+++ lib/modules/pokemon_details/ui/pages/pokemon_details.page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/modules/pokemon_details/ui/controllers/pokemon_details.controller.dart';
 import 'package:pokedex/modules/pokemon_list/infra/dtos/pokemon_dto.dart';
@@ -12,37 +13,85 @@ class PokemonDetailsPage extends StatelessWidget {
     PokemonDetailsController _controller = PokemonDetailsController();
     final args = ModalRoute.of(context)!.settings.arguments as PokemonDTO;
     Color? cardColor = _controller.getColorCard(args.type![0]);
-    return Container(
-      width: 100,
-      color: cardColor,
-      child: Column(
-        children: [
-          Text('${args.name}'),
-          Text('${args.id}'),
-          Text('${args.img}'),
-          Container(
-            width: 80,
-            height: 110,
-            child: ListView.builder(
-              itemCount: args.type?.length ?? 0,
-              itemBuilder: (context, i) {
-                return Container(
-                  margin: const EdgeInsets.all(3.0),
-                  padding: const EdgeInsets.all(3.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white10,
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                  ),
-                  child: Text(
-                    '${args.type![i]}',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                );
-              },
+    return Scaffold(
+      backgroundColor: cardColor,
+      body: Center(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 60.0),
+                      child: Text(
+                        '${args.name}',
+                        style: TextStyle(fontSize: 30, color: Colors.blueGrey),
+                      ),
+                    ),
+                    Container(
+                      width: 80,
+                      height: 100,
+                      child: ListView.builder(
+                        itemCount: args.type?.length ?? 0,
+                        itemBuilder: (context, i) {
+                          return Container(
+                            padding: const EdgeInsets.all(3.0),
+                            decoration: BoxDecoration(
+                              color: cardColor,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(12)),
+                            ),
+                            child: Text(
+                              '${args.type![i]}',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Text(
+                      'Height: ${args.height}',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16),
+                    ),
+                    Text(
+                      'Weight: ${args.weight}',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+            Align(
+              alignment: AlignmentDirectional.center,
+              child: Image.asset(
+                'assets/images/pokeball10w.png',
+                height: 200,
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Image.network(
+                '${args.img}',
+                width: MediaQuery.of(context).size.width * 0.7,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
